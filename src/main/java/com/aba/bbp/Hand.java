3d7912b11fc26cd0ac820076218b0a4d48291a2e
gregashby <ashbygreg@gmail.com>
2025-02-08 18:59:44 -0700

    handled detection of straight flush and made Hand comparable to determine highest hands

--- src/main/java/com/aba/bbp/Hand.java
+++ src/main/java/com/aba/bbp/Hand.java
@@ -4,7 +4,7 @@ import com.aba.bbp.enums.CardRank;
 import com.aba.bbp.enums.HandRank;
 import java.util.List;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
 
   public HandRank handRank;
   public List<CardRank> kickers;
@@ -12,5 +12,29 @@ public class Hand {
   public Hand(HandRank handRank, List<CardRank> kickers) {
 	this.handRank = handRank;
 	this.kickers = kickers;
+  }
+
+  @Override
+  public String toString() {
+	return handRank.toString() + " " + kickers.toString();
+  }
+
+  @Override
+  public int compareTo(Hand otherHand) {
+	if (this.handRank.ordinal() > otherHand.handRank.ordinal()) {
+	  return 1;
+	} else if (this.handRank.ordinal() < otherHand.handRank.ordinal()) {
+	  return -1;
+	}
+
+	for (int i = 0; i < this.kickers.size(); i++) {
+	  if (this.kickers.get(i).ordinal() > otherHand.kickers.get(i).ordinal()) {
+		return 1;
+	  } else if (this.kickers.get(i).ordinal() < otherHand.kickers.get(i).ordinal()) {
+		return -1;
+	  }
+	}
+
+	return 0;
   }
 }
---
+++ src/test/java/com/aba/bbp/HandTest.java
@@ -0,0 +1,104 @@
+package com.aba.bbp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.aba.bbp.enums.CardRank;
+import com.aba.bbp.enums.HandRank;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class HandTest {
+
+  static Stream<Arguments> provideHandsForComparison() {
+	return Stream.of(
+		// Comparison by HandRank
+		Arguments.of(
+			new Hand(HandRank.FLUSH,
+				List.of(CardRank.ACE, CardRank.KING, CardRank.QUEEN, CardRank.JACK, CardRank.TEN)),
+			new Hand(HandRank.STRAIGHT, List.of(CardRank.KING)),
+			// Straight is defined by its high card
+			1 // Flush > Straight
+		),
+		Arguments.of(
+			new Hand(HandRank.HIGH_CARD,
+				List.of(CardRank.ACE, CardRank.KING, CardRank.QUEEN, CardRank.JACK, CardRank.TEN)),
+			new Hand(HandRank.PAIR,
+				List.of(CardRank.TWO, CardRank.KING, CardRank.QUEEN, CardRank.JACK)),
+			// Pair uses 4 kickers: pair rank + 3 high cards
+			-1 // High Card < Pair
+		),
+		// High Card comparison using 5 kickers
+		Arguments.of(
+			new Hand(HandRank.HIGH_CARD,
+				List.of(CardRank.ACE, CardRank.KING, CardRank.QUEEN, CardRank.JACK, CardRank.TEN)),
+			new Hand(HandRank.HIGH_CARD,
+				List.of(CardRank.ACE, CardRank.KING, CardRank.QUEEN, CardRank.JACK, CardRank.NINE)),
+			1 // Ten kicker > Nine kicker
+		),
+		// Pair comparison using 4 kickers
+		Arguments.of(
+			new Hand(HandRank.PAIR,
+				List.of(CardRank.KING, CardRank.ACE, CardRank.QUEEN, CardRank.JACK)),
+			new Hand(HandRank.PAIR,
+				List.of(CardRank.KING, CardRank.ACE, CardRank.TEN, CardRank.NINE)),
+			1 // Queen kicker > Ten kicker
+		),
+		// Two Pair comparison using 3 kickers
+		Arguments.of(
+			new Hand(HandRank.TWO_PAIR, List.of(CardRank.KING, CardRank.TEN, CardRank.JACK)),
+			new Hand(HandRank.TWO_PAIR, List.of(CardRank.KING, CardRank.TEN, CardRank.NINE)),
+			1 // Jack kicker > Nine kicker
+		),
+		// Trips comparison (only 1 kicker needed since ties are impossible)
+		Arguments.of(
+			new Hand(HandRank.THREE_OF_A_KIND, List.of(CardRank.KING)),
+			new Hand(HandRank.THREE_OF_A_KIND, List.of(CardRank.QUEEN)),
+			1 // King trips > Queen trips
+		),
+		// Straight comparison using 1 kicker
+		Arguments.of(
+			new Hand(HandRank.STRAIGHT, List.of(CardRank.ACE)), // Ace-high straight
+			new Hand(HandRank.STRAIGHT, List.of(CardRank.KING)), // King-high straight
+			1 // Ace-high > King-high straight
+		),
+		// Full House comparison using 2 kickers
+		Arguments.of(
+			new Hand(HandRank.FULL_HOUSE, List.of(CardRank.KING, CardRank.TEN)),
+			// King-full-of-Tens
+			new Hand(HandRank.FULL_HOUSE, List.of(CardRank.KING, CardRank.NINE)),
+			// King-full-of-Nines
+			1 // Tens kicker > Nines kicker
+		),
+		// Quads comparison (only 1 kicker needed since ties are impossible)
+		Arguments.of(
+			new Hand(HandRank.FOUR_OF_A_KIND, List.of(CardRank.ACE)), // Four Aces
+			new Hand(HandRank.FOUR_OF_A_KIND, List.of(CardRank.KING)), // Four Kings
+			1 // Four Aces > Four Kings
+		),
+		// Straight Flush comparison (only 1 kicker needed since the rest are implied)
+		Arguments.of(
+			new Hand(HandRank.STRAIGHT_FLUSH, List.of(CardRank.ACE)), // Ace-high straight flush
+			new Hand(HandRank.STRAIGHT_FLUSH, List.of(CardRank.KING)), // King-high straight flush
+			1 // Ace-high > King-high straight flush
+		),
+		// Identical hands
+		Arguments.of(
+			new Hand(HandRank.HIGH_CARD,
+				List.of(CardRank.ACE, CardRank.KING, CardRank.QUEEN, CardRank.JACK, CardRank.TEN)),
+			new Hand(HandRank.HIGH_CARD,
+				List.of(CardRank.ACE, CardRank.KING, CardRank.QUEEN, CardRank.JACK, CardRank.TEN)),
+			0 // Both hands are equal
+		)
+	);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideHandsForComparison")
+  void testCompareTo(Hand hand1, Hand hand2, int expected) {
+	int actual = hand1.compareTo(hand2);
+	assertEquals(expected, actual);
+  }
+}
